c0c5f06c8d24aeba5eb3c3865473c8b58575107c
jttechno <jttechno22@gmail.com>
2026-02-02 23:02:03 -0500

    Added Location Tag
    Added a text UI in the bottom left that displays the current player region

---
+++ The Warehouse The Bare Minimum/Assets/Scripts/PlayerRegion.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerRegion : MonoBehaviour
+{
+    
+    public TextMeshProUGUI textMesh;
+    public string regionName;
+    
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Region"))
+        {
+            textMesh.text = "Sales Floor";
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Region"))
+        {
+            textMesh.text = (string)other.gameObject.name;
+        }
+        
+    }
+    
+}
