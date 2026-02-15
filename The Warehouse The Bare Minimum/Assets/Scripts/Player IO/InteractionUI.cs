85f7e1837ec1a0e8b5dc321ef243bf5e8efbccbd
jttechno <jttechno22@gmail.com>
2026-02-15 17:47:57 -0500

    Temp fix Interactions
    Made key presses for interactions more reliable.
    
    This brings up another issue though, making it so that all NPC on the map get interacted with when you interact w/ one

--- The Warehouse The Bare Minimum/Assets/Scripts/Player IO/InteractionUI.cs
+++ The Warehouse The Bare Minimum/Assets/Scripts/Player IO/InteractionUI.cs
@@ -6,26 +6,27 @@ public class InteractionUI : MonoBehaviour
 {
   
    public TextMeshProUGUI textMesh;
+   private bool inside;
 
-   void OnTriggerStay(Collider other)
-   {
-       if (other.gameObject.CompareTag("interUI"))
-       {
-           textMesh.text = "Press E to interact";
-           if (Input.GetKeyDown("e"))
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("player")){
+            inside = true;
+            textMesh.text = "Press E to interact";
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inside = false;
+        textMesh.text = "";
+    }
+
+    void Update()
+    {
+        if (inside = true && Input.GetKeyDown("e"))
             {
                 Debug.Log("You were in zone and pressed E");
             }
-       }
-      
-   }
-  
-   void OnTriggerExit(Collider other)
-   {
-       if (other.gameObject.CompareTag("interUI"))
-       {
-           textMesh.text = "";
-       }
-   }
-   
+    }
 }
