8b304ee3f03f8c1a0a24a7fe6d9d9f5015f8e7c1
jttechno <jttechno22@gmail.com>
2026-03-12 12:01:01 -0400

    Fixed Parking lot
    Fixed some collisions in the parking lot and added car movements.

--- The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Cart Run/movingCarsR.cs
+++ The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Cart Run/movingCarsR.cs
@@ -1,28 +1,30 @@
 using UnityEngine;
+using TMPro;
 
 public class movingCarsR: MonoBehaviour
 {
     public GameObject car;
+    public TextMeshProUGUI textMesh;
     private int baseSpeed = 5;
     private float modSpeed;
 
     //Creating the teleport function
     void carTeleport()
     {
-        car.transform.position = new Vector3 (-838,-492,-270);
+        car.transform.position = new Vector3 (-338,10,-275);
     }
     void FixedUpdate()
     {
         //Moving car to original side & setting a modified speed
-        if(car.transform.position.x >= 838)
+        if(car.transform.position.z >= 265)
         {
             carTeleport();
             modSpeed = baseSpeed + (Random.Range(0,6));
-            Debug.Log("Bottom lane speed: " + modSpeed);
+            textMesh.text = "" + modSpeed;
         }
 
         //Creating the variable to move the car
-        Vector3 carMove = new Vector3(modSpeed,0,0);
+        Vector3 carMove = new Vector3(0,0,modSpeed);
         
         //Moving the car
         car.transform.position += carMove;  
