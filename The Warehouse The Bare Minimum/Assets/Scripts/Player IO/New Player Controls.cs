47a71f7385400fff97c93bafb87ec6be0b4664b6
Gabe-B <gabeblack3@gmail.com>
2026-04-14 21:00:35 -0400

    working on player inputs

---
+++ The Warehouse The Bare Minimum/Assets/Scripts/Player IO/New Player Controls.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewPlayerControls : MonoBehaviour
+{
+    public InputActionAsset InputActions;
+
+    private InputAction m_moveAction;
+    private InputAction m_lookAction;
+    private InputAction m_jumpAction;
+}
