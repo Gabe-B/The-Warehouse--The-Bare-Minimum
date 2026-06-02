09530004c6d156a88be34ac4dedbbee5877082a3
Gabe-B <gabeblack3@gmail.com>
2026-06-01 20:59:27 -0400

    got local split screen working

---
+++ The Warehouse The Bare Minimum/Assets/Scripts/Player IO/Camera/CameraCollision.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    public float distance;
+
+    public Vector3 dollyDirectionAdjusted;
+    Vector3 dollyDirection;
+
+    LayerMask zones;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        dollyDirection = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+
+        zones = ~LayerMask.GetMask("Zone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDirection * maxDistance);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit, zones))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.4f), minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDirection * distance, Time.deltaTime * smooth);
+    }
+}
