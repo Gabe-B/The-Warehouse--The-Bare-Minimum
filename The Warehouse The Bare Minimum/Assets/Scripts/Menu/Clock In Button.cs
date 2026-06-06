a882677f7e67dc53bc8ee96670bb13be104013e8
jttechno <jttechno22@gmail.com>
2026-06-05 21:16:27 -0400

    Added pallet jack animation
    Added animation for pallet jack moving from off screen and loading next scene

---
+++ The Warehouse The Bare Minimum/Assets/Scripts/Menu/Clock In Button.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class clockInButton : MonoBehaviour
+{
+    public List<Scene> maps;
+    //Setting positions for the Lobby Jack
+    public GameObject lobbyJack;
+    public Transform startPosition,endPosition;
+    private float desiredDuration = 0.75f;
+    private float elapsedTime;
+    private bool buttonPressed;
+
+    public int randomSeed { get; private set; }
+
+
+	private void Awake()
+	{
+        randomSeed = Mathf.RoundToInt(Random.Range(-2147483648, 2147483648));
+
+        Random.InitState(randomSeed);
+        buttonPressed = false;
+    }
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            //Debug.Log(SceneManager.GetSceneByBuildIndex(i).name);
+            maps.Add(SceneManager.GetSceneByBuildIndex(i));
+        }
+        //Debug.Log((SceneManager.GetActiveScene().buildIndex + 1) + ", " + maps.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(buttonPressed == true)
+        {
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / desiredDuration;
+
+            lobbyJack.transform.position = Vector3.Lerp(startPosition.position,endPosition.position,percentageComplete);
+        }
+
+        if(lobbyJack.transform.position == endPosition.position)
+        {
+            int randomMap = Random.Range(SceneManager.GetActiveScene().buildIndex + 1, maps.Count + 1);
+
+            Debug.Log(randomMap);
+
+            SceneManager.LoadScene(randomMap);
+        }
+                
+    }
+    
+    public void OnStartPressed()
+    {
+        buttonPressed = true;
+        
+    }
+}
