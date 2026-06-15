5b9c72cedcdcf173c0cd41e3ecdecc1365eabe93
jttechno <jttechno22@gmail.com>
2026-06-15 00:44:07 -0400

    Added task initialization
    Added the beginning of task initialization.

---
+++ The Warehouse The Bare Minimum/Assets/Scripts/Tasks/laserLineManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class laserLineManager : MonoBehaviour
+{
+    public bool taskActive;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
