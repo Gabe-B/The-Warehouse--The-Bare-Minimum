3fc23bcce2e21d218308b7b8f8373dddc33465a7
jttechno <jttechno22@gmail.com>
2026-06-18 15:19:05 -0400

    Updated task
    Updated Hang TV task

--- The Warehouse The Bare Minimum/Assets/Testing/hammerTime.cs
+++ The Warehouse The Bare Minimum/Assets/Testing/hammerTime.cs
@@ -1,57 +1,81 @@
 using UnityEngine;
 using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
 
 public class hammerTime : MonoBehaviour
 {
+    //Setting variables and references
     public int fuckUps;
     public TextMeshProUGUI fUps;
     public Material material;
-    public GameObject nail1,nail2,nail3,nail4;
-    private bool nBool1, nBool2, nBool3, nBool4;
+    public GameObject tlNail, trNail, blNail, brNail;
+    private bool tlBool, trBool, blBool, brBool;
+    public GameObject playerCam, htvCanvas;
+    public Camera htvCam;
+    public CanvasGroup screenCrack;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         fuckUps = 3;
         fUps.text = fuckUps.ToString();
+        playerCam.SetActive(false);
+        htvCam.enabled = htvCam.enabled;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (nBool1&&nBool2&&nBool3&&nBool4)
+        if (tlBool && trBool && blBool && brBool)
         {
             Debug.Log("Nice Job the TV is hung!");
+            resetState();
         }
 
-        if (fuckUps<= 0)
+        if (fuckUps <= 0)
         {
             Debug.Log("Way to go you bungled the whole thing.");
+            resetState();
         }
     }
     public void onButtonClick()
     {
         fuckUps -= 1;
+        screenCrack.alpha += .1f;
         fUps.text = fuckUps.ToString();
     }
 
     public void TLN()
     {
-        nail1.GetComponent<MeshRenderer> ().material = material;
-        nBool1 = true;
+        tlNail.GetComponent<MeshRenderer>().material = material;
+        tlBool = true;
     }
     public void TRN()
     {
-        nail2.GetComponent<MeshRenderer> ().material = material;
-        nBool2 = true;
+        trNail.GetComponent<MeshRenderer>().material = material;
+        trBool = true;
     }
     public void BLN()
     {
-        nail3.GetComponent<MeshRenderer> ().material = material;
-        nBool3 = true;
+        blNail.GetComponent<MeshRenderer>().material = material;
+        blBool = true;
     }
     public void BRN()
     {
-        nail4.GetComponent<MeshRenderer> ().material = material;
-        nBool4 = true;
+        brNail.GetComponent<MeshRenderer>().material = material;
+        brBool = true;
+    }
+
+    public void resetState()
+    {
+        fuckUps = 3;
+        tlBool = trBool = blBool = brBool = false;
+        brNail.GetComponent<MeshRenderer>().material = material;
+        brNail.GetComponent<MeshRenderer>().material = material;
+        brNail.GetComponent<MeshRenderer>().material = material;
+        brNail.GetComponent<MeshRenderer>().material = material;
+        playerCam.SetActive(true);
+        htvCanvas.SetActive(false);
+        htvCam.enabled = !htvCam.enabled;
     }
 }
