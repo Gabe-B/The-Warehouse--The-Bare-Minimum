82c2c74384cb38c8d86acbddf11d1a6d680248e8
jttechno <jttechno22@gmail.com>
2026-07-02 19:21:57 -0400

    Updated tasks
    Finished the collection process
    
    Player(s) can:
    - pick up
    - throw
    - deposit

--- The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Carry out/Carry out.cs
+++ The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Carry out/Carry out.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class carryOut : MonoBehaviour
 {
     public Rigidbody rb;
+    public InteractionUI interUI;
     //If product is in zone then its collected and the customer can take off
     void OnTriggerEnter(Collider other)
     {
         //finds the rigid body of the product
+        GameObject interactionZone = other.transform.GetChild(0).gameObject;
         rb = other.gameObject.GetComponent<Rigidbody>();
+        Debug.Log("The item going into the collection zone: "+interactionZone.name);
+        // jrObject.gameObject.GetComponent<MeshRenderer>().enabled = true;
         if (other.gameObject.CompareTag("product"))
         {
             rb.isKinematic = true;
             other.transform.SetParent(transform, worldPositionStays: true);
             other.transform.localPosition = new Vector3 (0,0,0);
+            interactionZone.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            interactionZone.gameObject.GetComponent<SphereCollider>().enabled = false;
+            interUI.inHand = false;
         }
     }
     //Add a random chance of recieving a tip from a customer
