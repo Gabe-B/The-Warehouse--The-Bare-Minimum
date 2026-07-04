7835e97327b6635319de12b457469451c2a91b80
Gabe-B <gabeblack3@gmail.com>
2026-07-03 22:40:05 -0400

    Work on task manager

--- The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Laser Line/shelfManager.cs
+++ The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Laser Line/shelfManager.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 
-public class shelfManager : MonoBehaviour
+public class shelfManager : Task
 {
     public GameObject prOne;
     public GameObject prTwo;
     public GameObject prThree;
     public GameObject prFour;
-    
-    void Start()
+
+    public override void StartingTask()
     {
+        Debug.Log("The carryout has begun");
         prOne.tag = "dirty";
         prTwo.tag = "dirty";
         prThree.tag = "dirty";
         prFour.tag = "dirty";
+    }
+
+    public override void EndingTask()
+    {
+        Debug.Log("The carryout has ended");
+    }
+
+    void Start()
+    {
+        
     }
 }
--- The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Laser Line/shelfTaskManager.cs
+++ The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Laser Line/shelfTaskManager.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 
-public class shelfTaskManager : MonoBehaviour
+public class shelfTaskManager : IndividualTaskManager
 {
     public GameObject chosenShelf;
-    void Start()
-    {
-        GameObject[] shelves = GameObject.FindGameObjectsWithTag("shelf");
-        chosenShelf = shelves[Random.Range(0,shelves.Length)];
-        chosenShelf.GetComponent<shelfManager>().enabled = true;
-    }
+
+    public Task pairedShelf;
+
+	public override void StartTask()
+	{
+		base.StartTask();
+
+		GameObject[] shelves = GameObject.FindGameObjectsWithTag("shelf");
+		chosenShelf = shelves[Random.Range(0, shelves.Length)];
+		chosenShelf.GetComponent<shelfManager>().enabled = true;
+		pairedShelf.StartingTask();
+	}
+
+	public override void EndTask()
+	{
+		base.EndTask();
+
+		pairedShelf.EndingTask();
+	}
 }
