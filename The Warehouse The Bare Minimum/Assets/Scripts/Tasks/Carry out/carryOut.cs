a47e4ec94344c5c1aa0836407b9f9bb43aab2535
jttechno <jttechno22@gmail.com>
2026-07-10 19:40:52 -0400

    Added task and Tweaks
    New task: Curbside
    
    - Carry products from the store across the road to customer cars without being hit by traffic.
    
    Interactions changes:
    - New: Throw strength can now be charged to throw farther
    
    - Change: Products are now held in front of the player and rotate with the player naturally instead up being updated every frame

---
+++ The Warehouse The Bare Minimum/Assets/Scripts/Tasks/Carry out/carryOut.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class carryOut : Task
+{
+    private Rigidbody rb;
+    private InteractionUI interUI;
+
+    public override void StartingTask()
+    {
+        Debug.Log("The carryout has begun");
+    }
+
+    public override void EndingTask()
+    {
+        Debug.Log("The carryout has ended");
+    }
+
+    //If product is in zone then its collected and the customer can take off
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            interUI = other.GetComponent<InteractionUI>();
+        }
+        //finds the rigid body of the product
+        GameObject interactionZone = other.transform.GetChild(0).gameObject;
+        rb = other.gameObject.GetComponent<Rigidbody>();
+        Debug.Log("The item going into the collection zone: " + interactionZone.name);
+        // jrObject.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        if (other.gameObject.CompareTag("product"))
+        {
+            interUI.inHand = false;
+            rb.isKinematic = true;
+            other.transform.SetParent(transform, worldPositionStays: true);
+            other.transform.localPosition = new Vector3(0, 0, 0);
+            interactionZone.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            interactionZone.gameObject.GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+    //Add a random chance of recieving a tip from a customer
+}
